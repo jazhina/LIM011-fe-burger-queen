40e7ed08def6709706984b324eed52814ef99a34
cielolili <cielito.torres@hotmail.com>
2020-03-02 19:01:12 -0500

    practicando

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import React, { Component } from 'react';
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Vista1 from './components/vista1.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Vista1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/vista1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Vista1() {
+    return (
+      <div className="App">
+          <h1> Hola soy la vista 2! </h1>
+          <p> Wujuu</p>
+      </div>
+    );
+  }
+  
+  export default Vista1;
