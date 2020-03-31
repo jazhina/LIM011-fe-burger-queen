cf4f777ef54b8f0947363887b2bc52b4fae8923a
jazhina <jazmin12977@gmail.com>
2020-03-31 15:55:16 -0500

    Test componente order

--- src/test/title.spec.jsx
+++ src/test/title.spec.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Menu from '../components/menu';
+import Title from '../components/title';
 
 describe('Title', () => {
   it('Debería mostrar el logo y el titulo de la aplicacion', () => {
-    const container = render(<Menu />);
+    const container = render(<Title />);
     const eleLogo = container.getByTestId('logo');
-    expect(eleLogo).toBeInTheDocument();
+    expect(eleLogo).toBeTruthy();
   });
 });
